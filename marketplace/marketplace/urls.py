cc766f0961dca2891a6a18cfa10ebd8306c6dff1
zorourke14 <zorourke14@gmail.com>
2023-11-14 12:21:38 -0500

    added items app and changed home page

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = "index"),
     path('contact/', views.contact, name ="contact")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
